94011bcac98cd5d2a0f7761980f51ca9b608df66
RuthRadekta <ruthradekta@student.uns.ac.id>
2024-01-28 22:39:13 +0700

    update
    (-) something weird when media <600px

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,3 @@
-// slideshow.js
-let currentTestimonial = 0;
-const testimonialSlides = document.querySelectorAll('.testimonial-slide');
-
-function showTestimonial(index) {
-    testimonialSlides.forEach((slide, i) => {
-        if (i === index) {
-            slide.style.display = 'block';
-        } else {
-            slide.style.display = 'none';
-        }
-    });
-}
-
-function nextTestimonial() {
-    currentTestimonial = (currentTestimonial + 1) % testimonialSlides.length;
-    showTestimonial(currentTestimonial);
-}
-
-// Set interval for automatic testimonial slideshow (change testimonial every 5 seconds)
-setInterval(nextTestimonial, 5000);
-
-// Initial display
-showTestimonial(currentTestimonial);
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const faqItems = document.querySelectorAll('.faq-item');
     faqItems.forEach(item => {
@@ -32,3 +6,4 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
